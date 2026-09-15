ac99e3cf55e139f893199f0c5b2e2fa1dd534f31
cdmi4613 <cdmi4613@naver.com>
2026-09-15 10:08:35 +0900

    Deploy to ASG

--- user-data.sh
+++ user-data.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 dnf update -y
-
-# Docker 설치
 dnf install -y docker
 
-# Docker 시작 + 부팅 시 자동 시작
 systemctl enable --now docker
-
-# ec2-user Docker 권한
 usermod -aG docker ec2-user
 
 # Docker Compose 설치
 mkdir -p /usr/local/lib/docker/cli-plugins
-
 curl -SL \
   https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
   -o /usr/local/lib/docker/cli-plugins/docker-compose
